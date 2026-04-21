6371fd359cdb351e7b6dae7f54998e3a7fb7750f
Astrid Rosal <astridjamesrosal95@gmail.com>
2026-04-21 15:16:51 +0800

    Added 10 Questions about Information Technology

---
+++ quizapp.py
@@ -0,0 +1,55 @@
+questions = [
+    {
+        "question": "This is the brain of the computer and is responsible for carrying out instructions and running calculations.",
+        "choices": ["A. Random Access Memory", "B. System Board", "C. Central Processing Unit", "D. Graphics Processing Unit"],
+        "answer": "C. Central Processing Unit"
+    },
+    {
+        "question": "This organises data by storing similar elements and allocating contiguous memory.",
+        "choices": ["A. Queues", "B. Arrays", "C. Stacks", "D. Binary Tree"],
+        "answer": "B. Arrays"
+    },
+    {
+        "question": "An operating system that manages the execution of multiple processes, including tasks such as scheduling, synchronization, and communication.",
+        "choices": ["A. File System Management", "B. Device Management", "C. Security Access Controls", "D. Process Management"],
+        "answer": "D. Process Management"
+    },
+    {
+        "question": "Facilitates data communication between the system and external devices. it handles data transfer, manage device access, and ensures error-free communication.",
+        "choices": ["A. Input/Output Manager", "B. Memory Manager", "C. Kernel", "D. File System Manager"],
+        "answer": "A. Input/Output Manager"
+    },
+    {
+        "question": "A type of database that store it in defined tables with rows and columns. Excels with structured data as financial records.",
+        "choices": ["A. Cloud Database", "B. Relational Database", "C. Non-relational Database", "D. Navigational Database"],
+        "answer": "B. Relational Database"
+    },
+    {
+        "question": "A type of database that stores it as a variety of data structures, including key-value pairs or graphs. Best for unstructured data types such as text files, audio, and video.",
+        "choices": ["A. Cloud Database", "B. Relational Database", "C. Non-relational Database", "D. Navigational Database"],
+        "answer": "C. Non-relational Database"
+    },
+    {
+        "question": "A unique number assigned to every network device. Identifies the device's host network and its location on the network.",
+        "choices": ["A. Routers", "B. IP Address", "C. Switches", "D. Gateways"],
+        "answer": "B. IP Address"
+    },
+    {
+        "question": "They provide isolated network environments within a public cloud infrastructure.",
+        "choices": ["A. Virtual Private Clouds", "B. Content Delivery Networks", "C. Service Mesh", "D. Load Balancers"],
+        "answer": "A. Virtual Private Clouds"
+    },
+    {
+        "question": "This Category of cybersecurity focuses on keeping applications and devices free of threats",
+        "choices": ["A. Network Security", "B. Operational Security", "C. Information Security", "D. Application Security"],
+        "answer": "D. Application Security"
+    },
+    {
+        "question": "This is a type of software designed to gain unauthorized access or to cause damage to a computer.",
+        "choices": ["A. Phishing", "B. Malware", "C. SQL Injection", "D. Social Engineering"],
+        "answer": "B. Malware"
+    }
+]
+
+
+    
